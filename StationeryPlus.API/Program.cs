782e0a6fd14a0d1edc0e729937320449df8b41f7
sumi-bhatta <166995225+sumi-bhatta@users.noreply.github.com>
2024-07-07 10:18:51 +0545

    Conflict Resolved

--- StationeryPlus.API/Program.cs
+++ StationeryPlus.API/Program.cs
@@ -1,9 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using StationeryPlus.DataAccess.Data;
-
-var builder = WebApplication.CreateBuilder(args);
 using StationeryPlus.Business.Misc;
-using StationeryPlus.DataAccess.Data;
 using StationeryPlus.IBusiness.Misc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,8 +10,6 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDBContext>(options=>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
-
-var app = builder.Build();
 
 builder.Services.AddScoped<IStationeryPlusService, StationeryPlusService>();
 var app = builder.Build();
